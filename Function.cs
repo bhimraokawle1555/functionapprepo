631374f3040fc34ce164e4d18fbde692f3292779
Rahul Kasar <Rahul.Kasar@vault-solutions.com>
2024-07-01 17:05:47 +0530

    test

---
+++ Function.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionApp
+{
+    public class Function
+    {
+        private readonly ILogger _logger;
+
+        public Function(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Function>();
+        }
+
+        [Function("Function")]
+        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+        {
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
+        }
+    }
+}
